bcc4605f4aa11b78758285c9ae7f3bae972c288f
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-12-16 01:32:30 +0800

    Remove create fn

--- api/user-account/controllers/user-account.js
+++ api/user-account/controllers/user-account.js
@@ -8,19 +8,6 @@
 const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
-  // Create user account with linked user
-  async create(ctx) {
-    let entity;
-    if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-      data.user = ctx.state.user.id;
-      entity = await strapi.services['user-account'].create(data, { files });
-    } else {
-      ctx.request.body.user = ctx.state.user.id;
-      entity = await strapi.services['user-account'].create(ctx.request.body);
-    }
-    return sanitizeEntity(entity, { model: strapi.models['user-account'] });
-  },
   async update(ctx) {
     const { id } = ctx.params;
 
